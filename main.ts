2856d556149907245465d088f27adc0f042460db
Jordan Burgess <jordan@humanloop.com>
2022-11-09 13:53:56 +0000

    Add React

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 import {
-	addIcon,
-	App,
-	Editor,
-	MarkdownView,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	request,
-	Setting,
+  addIcon,
+  App,
+  Editor,
+  MarkdownView,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  request,
+  Setting,
 } from "obsidian";
 import { ExampleView, VIEW_TYPE_EXAMPLE } from "view";
 
@@ -18,330 +18,320 @@ const appPencile_icon = `<defs><style>.cls-1{fill:none;stroke:currentColor;strok
 // Remember to rename these classes and interfaces!
 
 interface TextGeneratorSettings {
-	openai_api_key: string;
-	humanloop_api_key: string;
-	context: string;
-	showStatusBar: boolean;
-	max_tokens: number;
+  openai_api_key: string;
+  humanloop_api_key: string;
+  context: string;
+  showStatusBar: boolean;
+  max_tokens: number;
 }
 
 const DEFAULT_SETTINGS: TextGeneratorSettings = {
-	openai_api_key: "",
-	humanloop_api_key: "sk_62845aad20664ad37dea7b9513ca97b5",
-	context: "",
-	showStatusBar: true,
-	max_tokens: 256,
+  openai_api_key: "",
+  humanloop_api_key: "sk_62845aad20664ad37dea7b9513ca97b5",
+  context: "",
+  showStatusBar: true,
+  max_tokens: 256,
 };
 
 export default class TextGeneratorPlugin extends Plugin {
-	settings: TextGeneratorSettings;
-	statusBarItemEl: any;
+  settings: TextGeneratorSettings;
+  statusBarItemEl: any;
 
-	async getGeneratedText(reqParams: any) {
-		let requestResults;
-		try {
-			requestResults = JSON.parse(await request(reqParams));
-		} catch (error) {
-			console.log(error);
-			return Promise.reject(error);
-		}
-		const text = requestResults?.logs[0].output;
-		return text;
-	}
+  async getGeneratedText(reqParams: any) {
+    let requestResults;
+    try {
+      requestResults = JSON.parse(await request(reqParams));
+    } catch (error) {
+      console.log(error);
+      return Promise.reject(error);
+    }
+    const text = requestResults?.logs[0].output;
+    return text;
+  }
 
-	getActiveView() {
-		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		if (activeView !== null) {
-			return activeView;
-		} else {
-			new Notice("The file type should be Markdown!");
-			return null;
-		}
-	}
+  getActiveView() {
+    const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+    if (activeView !== null) {
+      return activeView;
+    } else {
+      new Notice("The file type should be Markdown!");
+      return null;
+    }
+  }
 
-	insertGeneratedText(text: string, editor: Editor) {
-		let cursor = editor.getCursor();
+  insertGeneratedText(text: string, editor: Editor) {
+    let cursor = editor.getCursor();
 
-		if (editor.listSelections().length > 0) {
-			const anchor = editor.listSelections()[0].anchor;
-			const head = editor.listSelections()[0].head;
-			if (
-				anchor.line > head.line ||
-				(anchor.line === head.line && anchor.ch > head.ch)
-			) {
-				cursor = editor.listSelections()[0].anchor;
-			}
-		}
-		editor.replaceRange(text, cursor);
-	}
+    if (editor.listSelections().length > 0) {
+      const anchor = editor.listSelections()[0].anchor;
+      const head = editor.listSelections()[0].head;
+      if (
+        anchor.line > head.line ||
+        (anchor.line === head.line && anchor.ch > head.ch)
+      ) {
+        cursor = editor.listSelections()[0].anchor;
+      }
+    }
+    editor.replaceRange(text, cursor);
+  }
 
-	/*
+  /*
 	Prepare the request parameters
 	*/
 
-	prepareParameters(
-		params: TextGeneratorSettings,
-		project_name: string = "Extend",
-		editor: Editor
-	) {
-		params = {
-			...params,
-			context: this.getContext(editor),
-		};
+  prepareParameters(
+    params: TextGeneratorSettings,
+    project_name: string = "Extend",
+    editor: Editor
+  ) {
+    params = {
+      ...params,
+      context: this.getContext(editor),
+    };
 
-		let bodyParams: any = {
-			project: project_name,
-			max_tokens: params.max_tokens,
-			inputs: { input: params.context },
-			provider_api_keys: {
-				OpenAI: params.openai_api_key,
-			},
-		};
+    let bodyParams: any = {
+      project: project_name,
+      max_tokens: params.max_tokens,
+      inputs: { input: params.context },
+      provider_api_keys: {
+        OpenAI: params.openai_api_key,
+      },
+    };
 
-		console.log(bodyParams);
+    console.log(bodyParams);
 
-		let reqParams = {
-			url: "https://api.humanloop.com/v1/generate",
-			method: "POST",
-			body: JSON.stringify(bodyParams),
-			headers: {
-				"Content-Type": "application/json",
-				"X-API-KEY": params.humanloop_api_key,
-			},
-		};
-		return reqParams;
-	}
+    let reqParams = {
+      url: "https://api.humanloop.com/v1/generate",
+      method: "POST",
+      body: JSON.stringify(bodyParams),
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": params.humanloop_api_key,
+      },
+    };
+    return reqParams;
+  }
 
-	async generate(
-		params: TextGeneratorSettings,
-		project_name: string = "Extend",
-		editor: Editor
-	) {
-		const parameters = this.prepareParameters(params, project_name, editor);
-		let text;
-		try {
-			text = await this.getGeneratedText(parameters);
-		} catch (error) {
-			return Promise.reject(error);
-		}
+  async generate(
+    params: TextGeneratorSettings,
+    project_name: string = "Extend",
+    editor: Editor
+  ) {
+    const parameters = this.prepareParameters(params, project_name, editor);
+    let text;
+    try {
+      text = await this.getGeneratedText(parameters);
+    } catch (error) {
+      return Promise.reject(error);
+    }
 
-		this.insertGeneratedText(text, editor);
-	}
+    this.insertGeneratedText(text, editor);
+  }
 
-	getContext(editor: Editor) {
-		let selectedText = editor.getSelection();
-		if (selectedText.length === 0) {
-			const lineNumber = editor.getCursor().line;
-			selectedText = editor.getLine(lineNumber);
-			if (selectedText.length === 0) {
-				selectedText = editor.getValue();
-			}
-		}
-		return selectedText;
-	}
+  getContext(editor: Editor) {
+    let selectedText = editor.getSelection();
+    if (selectedText.length === 0) {
+      const lineNumber = editor.getCursor().line;
+      selectedText = editor.getLine(lineNumber);
+      if (selectedText.length === 0) {
+        selectedText = editor.getValue();
+      }
+    }
+    return selectedText;
+  }
 
-	updateStatusBar(text: string) {
-		let text2 = "";
-		if (text.length > 0) {
-			text2 = `: ${text}`;
-		}
+  updateStatusBar(text: string) {
+    let text2 = "";
+    if (text.length > 0) {
+      text2 = `: ${text}`;
+    }
 
-		if (this.settings.showStatusBar) {
-			this.statusBarItemEl.setText(`Text Generating'${text2}`);
-		}
-	}
+    if (this.settings.showStatusBar) {
+      this.statusBarItemEl.setText(`Text Generating'${text2}`);
+    }
+  }
 
-	async onload() {
-		addIcon("pencil_icon", pencil_icon);
-		addIcon("appPencile_icon", appPencile_icon);
+  async onload() {
+    addIcon("pencil_icon", pencil_icon);
+    addIcon("appPencile_icon", appPencile_icon);
 
-		this.registerView(VIEW_TYPE_EXAMPLE, (leaf) => new ExampleView(leaf));
-		this.addRibbonIcon("dice", "Activate view", () => {
-			this.activateView();
-		});
-		await this.loadSettings();
-		this.statusBarItemEl = this.addStatusBarItem();
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"pencil_icon",
-			"Generate Text!",
-			async (evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				const activeFile = this.app.workspace.getActiveFile();
-				this.updateStatusBar(`processing... `);
-				const activeView = this.getActiveView();
-				if (activeView !== null) {
-					const editor = activeView.editor;
-					try {
-						await this.generate(this.settings, "Extend", editor);
-						this.updateStatusBar(``);
-					} catch (error) {
-						new Notice(
-							"Text Generator Plugin: Error check console CTRL+SHIFT+I"
-						);
-						this.updateStatusBar(`Error: Check Console`);
-						setTimeout(() => this.updateStatusBar(``), 3000);
-					}
-				}
-			}
-		);
-		this.addCommand({
-			id: "open-view",
-			name: "Open ThoughtPartner",
-			icon: "pencil_icon",
-			editorCallback: async (editor: Editor) => {
-				this.activateView();
-			},
-		});
+    this.registerView(VIEW_TYPE_EXAMPLE, (leaf) => new ExampleView(leaf));
+    this.addRibbonIcon("dice", "Activate view", () => {
+      this.activateView();
+    });
+    await this.loadSettings();
+    this.statusBarItemEl = this.addStatusBarItem();
+    // This creates an icon in the left ribbon.
+    const ribbonIconEl = this.addRibbonIcon(
+      "pencil_icon",
+      "Generate Text!",
+      async (evt: MouseEvent) => {
+        // Called when the user clicks the icon.
+        const activeFile = this.app.workspace.getActiveFile();
+        this.updateStatusBar(`processing... `);
+        const activeView = this.getActiveView();
+        if (activeView !== null) {
+          const editor = activeView.editor;
+          try {
+            await this.generate(this.settings, "Extend", editor);
+            this.updateStatusBar(``);
+          } catch (error) {
+            new Notice(
+              "Text Generator Plugin: Error check console CTRL+SHIFT+I"
+            );
+            this.updateStatusBar(`Error: Check Console`);
+            setTimeout(() => this.updateStatusBar(``), 3000);
+          }
+        }
+      }
+    );
+    this.addCommand({
+      id: "open-view",
+      name: "Open ThoughtPartner",
+      icon: "pencil_icon",
+      editorCallback: async (editor: Editor) => {
+        this.activateView();
+      },
+    });
 
-		this.addCommand({
-			id: "extend-text",
-			name: "keep writing",
-			icon: "pencil_icon",
-			hotkeys: [{ modifiers: ["Ctrl"], key: "j" }],
-			editorCallback: async (editor: Editor) => {
-				this.updateStatusBar(`processing... `);
-				try {
-					await this.generate(this.settings, "Extend", editor);
-					this.updateStatusBar(``);
-				} catch (error) {
-					new Notice(
-						"Text Generator Plugin: Error check console CTRL+SHIFT+I"
-					);
-					this.updateStatusBar(`Error check console`);
-					setTimeout(() => this.updateStatusBar(``), 3000);
-				}
-			},
-		});
+    this.addCommand({
+      id: "extend-text",
+      name: "keep writing",
+      icon: "pencil_icon",
+      hotkeys: [{ modifiers: ["Ctrl"], key: "j" }],
+      editorCallback: async (editor: Editor) => {
+        this.updateStatusBar(`processing... `);
+        try {
+          await this.generate(this.settings, "Extend", editor);
+          this.updateStatusBar(``);
+        } catch (error) {
+          new Notice("Text Generator Plugin: Error check console CTRL+SHIFT+I");
+          this.updateStatusBar(`Error check console`);
+          setTimeout(() => this.updateStatusBar(``), 3000);
+        }
+      },
+    });
 
-		this.addCommand({
-			id: "summarise",
-			name: "tl:dr",
-			icon: "appPencile_icon",
-			hotkeys: [{ modifiers: ["Ctrl"], key: "t" }],
-			editorCallback: async (editor: Editor) => {
-				this.updateStatusBar(`processing... `);
-				try {
-					await this.generate(this.settings, "summarise", editor);
-					this.updateStatusBar(``);
-				} catch (error) {
-					new Notice(
-						"Text Generator Plugin: Error check console CTRL+SHIFT+I"
-					);
-					this.updateStatusBar(`Error check console`);
-					setTimeout(() => this.updateStatusBar(``), 3000);
-				}
-			},
-		});
+    this.addCommand({
+      id: "summarise",
+      name: "tl;dr",
+      icon: "appPencile_icon",
+      hotkeys: [{ modifiers: ["Ctrl"], key: "t" }],
+      editorCallback: async (editor: Editor) => {
+        this.updateStatusBar(`processing... `);
+        try {
+          await this.generate(this.settings, "summarise", editor);
+          this.updateStatusBar(``);
+        } catch (error) {
+          new Notice("Text Generator Plugin: Error check console CTRL+SHIFT+I");
+          this.updateStatusBar(`Error check console`);
+          setTimeout(() => this.updateStatusBar(``), 3000);
+        }
+      },
+    });
 
-		this.addCommand({
-			id: "critique",
-			name: "critique",
-			icon: "appPencile_icon",
-			hotkeys: [{ modifiers: ["Ctrl"], key: "q" }],
-			editorCallback: async (editor: Editor) => {
-				this.updateStatusBar(`processing... `);
-				try {
-					await this.generate(this.settings, "critique", editor);
-					this.updateStatusBar(``);
-				} catch (error) {
-					new Notice(
-						"Text Generator Plugin: Error check console CTRL+SHIFT+I"
-					);
-					this.updateStatusBar(`Error check console`);
-					setTimeout(() => this.updateStatusBar(``), 3000);
-				}
-			},
-		});
+    this.addCommand({
+      id: "critique",
+      name: "critique",
+      icon: "appPencile_icon",
+      hotkeys: [{ modifiers: ["Ctrl"], key: "q" }],
+      editorCallback: async (editor: Editor) => {
+        this.updateStatusBar(`processing... `);
+        try {
+          await this.generate(this.settings, "critique", editor);
+          this.updateStatusBar(``);
+        } catch (error) {
+          new Notice("Text Generator Plugin: Error check console CTRL+SHIFT+I");
+          this.updateStatusBar(`Error check console`);
+          setTimeout(() => this.updateStatusBar(``), 3000);
+        }
+      },
+    });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new TextGeneratorSettingTab(this.app, this));
-	}
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    this.addSettingTab(new TextGeneratorSettingTab(this.app, this));
+  }
 
-	onunload() {
-		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
-	}
+  onunload() {
+    this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-	async activateView() {
-		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+  async activateView() {
+    this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
 
-		await this.app.workspace.getRightLeaf(false).setViewState({
-			type: VIEW_TYPE_EXAMPLE,
-			active: true,
-		});
+    await this.app.workspace.getRightLeaf(false).setViewState({
+      type: VIEW_TYPE_EXAMPLE,
+      active: true,
+    });
 
-		this.app.workspace.revealLeaf(
-			this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0]
-		);
-	}
+    this.app.workspace.revealLeaf(
+      this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0]
+    );
+  }
 }
 
 class TextGeneratorSettingTab extends PluginSettingTab {
-	plugin: TextGeneratorPlugin;
+  plugin: TextGeneratorPlugin;
 
-	constructor(app: App, plugin: TextGeneratorPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: TextGeneratorPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const { containerEl } = this;
+  display(): void {
+    const { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		containerEl.createEl("h2", {
-			text: "Settings for OpenAI",
-		});
+    containerEl.createEl("h2", {
+      text: "Settings for OpenAI",
+    });
 
-		new Setting(containerEl)
-			.setName("OpenAI API Key")
-			.setDesc("Set your OpenAI API Key. Go to https://beta.openai.com/")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your api_key")
-					.setValue(this.plugin.settings.openai_api_key)
-					.onChange(async (value) => {
-						this.plugin.settings.openai_api_key = value;
-						await this.plugin.saveSettings();
-					})
-			);
+    new Setting(containerEl)
+      .setName("OpenAI API Key")
+      .setDesc("Set your OpenAI API Key. Go to https://beta.openai.com/")
+      .addText((text) =>
+        text
+          .setPlaceholder("Enter your api_key")
+          .setValue(this.plugin.settings.openai_api_key)
+          .onChange(async (value) => {
+            this.plugin.settings.openai_api_key = value;
+            await this.plugin.saveSettings();
+          })
+      );
 
-		new Setting(containerEl)
-			.setName("Humanloop API Key")
-			.setDesc(
-				"Set your Humanloop API Key. Go to https://app.humanloop.com/settings to get it."
-			)
-			.addText((text) =>
-				text
-					.setPlaceholder("Set your humanlo api_key")
-					.setValue(this.plugin.settings.humanloop_api_key)
-					.onChange(async (value) => {
-						this.plugin.settings.humanloop_api_key = value;
-						await this.plugin.saveSettings();
-					})
-			);
+    new Setting(containerEl)
+      .setName("Humanloop API Key")
+      .setDesc(
+        "Set your Humanloop API Key. Go to https://app.humanloop.com/settings to get it."
+      )
+      .addText((text) =>
+        text
+          .setPlaceholder("Set your humanlo api_key")
+          .setValue(this.plugin.settings.humanloop_api_key)
+          .onChange(async (value) => {
+            this.plugin.settings.humanloop_api_key = value;
+            await this.plugin.saveSettings();
+          })
+      );
 
-		new Setting(containerEl)
-			.setName("showStatusBar")
-			.setDesc("Show information in the Status Bar")
-			.addToggle((v) =>
-				v
-					.setValue(this.plugin.settings.showStatusBar)
-					.onChange(async (value) => {
-						this.plugin.settings.showStatusBar = value;
-						await this.plugin.saveSettings();
-					})
-			);
-	}
+    new Setting(containerEl)
+      .setName("showStatusBar")
+      .setDesc("Show information in the Status Bar")
+      .addToggle((v) =>
+        v
+          .setValue(this.plugin.settings.showStatusBar)
+          .onChange(async (value) => {
+            this.plugin.settings.showStatusBar = value;
+            await this.plugin.saveSettings();
+          })
+      );
+  }
 }
